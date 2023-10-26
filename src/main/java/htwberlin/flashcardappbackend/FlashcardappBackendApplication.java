1f069a9aecd8dfbe0120250fe653a5d8b4d07014
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2023-10-26 13:20:37 +0200

    first commit - hello world

---
+++ src/main/java/htwberlin/flashcardappbackend/FlashcardappBackendApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.flashcardappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/flashcardappbackend/FlashcardappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.flashcardappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
